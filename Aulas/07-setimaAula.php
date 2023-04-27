677953a2a7b1dd07cd5f50a807832a2e4f4b5dcc
Camila Oliveira <devcamilasouza@gmail.com>
2023-04-27 20:34:01 -0300

    Adiciona aula7 - banco de dados

--- Aulas/07-setimaAula.php
+++ Aulas/07-setimaAula.php
@@ -1,21 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 06</title>
+    <title>Aula 07</title>
 </head>
+
 <body>
-    <h1><?php echo "Utilizando Banco de Dados";?></h1>
+    <h1>
+        <?php echo "Utilizando Banco de Dados"; ?>
+    </h1>
 
     <?php
-     
+    $servername = "localhost";
+    $username = "root";
+    $password = " ";
+    $dbname = "si3a";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ($conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "select * from estado";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = msqli_fetch_assoc($result)){
+            echo "id: " . $row["idEstado"] . " - Name" . $row["nomeEstado"] . "<br>";
+        }
+    }else{
+        echo "0 result";
+    }
+    echo "Connected successfully";
     ?>
 
-    <?php
-     
-    ?>
-    
 </body>
+
 </html>
